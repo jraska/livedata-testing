6b90231c88cfe400eab263575befe6223bd923b3
Josef Raska <6277721+jraska@users.noreply.github.com>
2018-08-26 16:14:37 +0200

    Fix build error

--- testing/src/test/java/com/jraska/livedata/TestObserverTest.kt
+++ testing/src/test/java/com/jraska/livedata/TestObserverTest.kt
@@ -1,7 +1,7 @@
 package com.jraska.livedata
 
-import android.arch.core.executor.testing.InstantTaskExecutorRule
-import android.arch.lifecycle.MutableLiveData
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
