ffb805ecbdc5082bdc83f06f25f026daf11fd96d
Josef Raska <6277721+jraska@users.noreply.github.com>
2018-08-26 16:00:29 +0200

    Migrate to android ktx

--- testing-ktx/src/main/java/com/jraska/livedata/TestObserver.kt
+++ testing-ktx/src/main/java/com/jraska/livedata/TestObserver.kt
@@ -1,6 +1,6 @@
 package com.jraska.livedata
 
-import android.arch.lifecycle.LiveData
+import androidx.lifecycle.LiveData
 
 fun <T> LiveData<T>.test(): TestObserver<T> {
   return TestObserver.test(this)
